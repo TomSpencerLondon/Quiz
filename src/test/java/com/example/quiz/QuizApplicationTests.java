784ca21516f4bc977d6c9daf37950e5ee0b8d7cf
Tom <thomas.spencer@codurance.com>
2021-10-04 15:40:59 +0100

    Add

---
+++ src/test/java/com/example/quiz/QuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
