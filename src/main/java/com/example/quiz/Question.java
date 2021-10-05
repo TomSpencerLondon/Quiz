98b28eaa4868e50da3b2b8c89153c1e43dea1a05
Tom <thomas.spencer@codurance.com>
2021-10-05 19:14:10 +0100

    Add knows_OneAnswer for Question

--- src/main/java/com/example/quiz/Question.java
+++ src/main/java/com/example/quiz/Question.java
@@ -1,13 +1,20 @@
 package com.example.quiz;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static java.util.Arrays.asList;
 import static org.apache.commons.lang3.builder.EqualsBuilder.reflectionEquals;
 import static org.apache.commons.lang3.builder.HashCodeBuilder.reflectionHashCode;
 
 public class Question {
     private String text;
+    private List<String> answers = new ArrayList<>();
 
-    public Question(String text) {
+    public Question(String text, String... answers) {
         this.text = text;
+        this.answers.addAll(asList(answers));
     }
 
     @Override
@@ -23,5 +30,9 @@ public class Question {
     @Override
     public String toString() {
         return this.text;
+    }
+
+    public List<String> answers() {
+        return answers;
     }
 }
---
+++ src/test/java/com/example/quiz/QuestionTest.java
@@ -0,0 +1,33 @@
+package com.example.quiz;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class QuestionTest {
+    @Test
+    void knows_noAnswers() {
+        // Given
+        Question question = new Question("Question 1");
+
+        // When
+        List<String> answers = question.answers();
+
+        // Then
+        assertThat(answers).isEmpty();
+    }
+
+    @Test
+    void knows_OneAnswer() {
+        // Given
+        Question question = new Question("Question 1", "Answer 1");
+
+        // When
+        List<String> answers = question.answers();
+
+        // Then
+        assertThat(answers).containsExactly("Answer 1");
+    }
+}
