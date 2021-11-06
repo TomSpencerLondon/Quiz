6b0c3605fb5a13015b4c712a8f5fab214464c903
Tom <thomas.spencer@codurance.com>
2021-11-06 17:42:39 +0000

    Refactor tests to use explanatory parameterized tests

--- src/test/java/com/example/quiz/adapter/in/questionloader/QuestionLoaderTest.java
+++ src/test/java/com/example/quiz/adapter/in/questionloader/QuestionLoaderTest.java
@@ -1,8 +1,9 @@
 package com.example.quiz.adapter.in.questionloader;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 import com.example.quiz.domain.Answer;
+import com.example.quiz.domain.MultipleChoice;
 import com.example.quiz.domain.Question;
 import java.util.List;
 import org.junit.jupiter.api.Test;
@@ -12,102 +13,69 @@ public class QuestionLoaderTest {
   @Test
   void changes_question_text_to_a_question() {
     String inputText = """
-        Using a Callable would be more appropriate than using a Runnable in which of the following situations?
+        Q1
                 
-        When you want to execute a task directly in a separate thread
-        When your task might throw a checked exception and you want to execute it in a separate Thread
-        When your task does not return any result but you want to execute the task asynchronously.
-        When you want to use ExecutorService to submit multiple instances of a task.
+        Q1A1
+        Q1A2
+        Q1A3
+        Q1A4
                 
-        When your task might throw a checked exception and you want to execute it in a separate Thread.
+        Q1A2
         """;
     final QuestionLoader questionLoader = new QuestionLoader();
 
     List<Question> questions = questionLoader.parse(inputText);
 
-    assertThat(questions.get(0).text())
-        .isEqualTo(
-            "Using a Callable would be more appropriate than using a Runnable in which of the following situations?");
+    final Question question1 = createQuestion("Q1", "Q1A2", "Q1A1", "Q1A2", "Q1A3", "Q1A4");
 
-    assertThat(questions.get(0).answers().containsAll(
-        List.of(new Answer("When you want to execute a task directly in a separate thread"),
-            new Answer(
-                "When your task might throw a checked exception and you want to execute it in a separate Thread"),
-            new Answer(
-                "When your task does not return any result but you want to execute the task asynchronously."),
-            new Answer(
-                "When you want to use ExecutorService to submit multiple instances of a task."))
-    ));
-
-    assertThat(questions.get(0).correctAnswer())
-        .isEqualTo(new Answer(
-            "When your task might throw a checked exception and you want to execute it in a separate Thread."));
+    assertThat(questions)
+        .containsExactly(question1);
   }
 
   @Test
   void parsesTwoQuestions() {
     String inputText = """
-        Using a Callable would be more appropriate than using a Runnable in which of the following situations?
+        Q1
                 
-        When you want to execute a task directly in a separate thread
-        When your task might throw a checked exception and you want to execute it in a separate Thread
-        When your task does not return any result but you want to execute the task asynchronously.
-        When you want to use ExecutorService to submit multiple instances of a task.
+        Q1A1
+        Q1A2
+        Q1A3
+        Q1A4
                 
-        When your task might throw a checked exception and you want to execute it in a separate Thread.
-        
+        Q1A2
+                
         ---
-        
-        Given that a code fragment has just created a JDBC Connection and has executed an update statement, which of the following statements is correct?
-        
-        Changes to the database are pending a commit call on the connection.
-        Changes to the database will be rolled back if another update is executed without commiting the previous update.
-        Changes to the database will be committed right after the update statement has completed execution.
-        A Connection is always in auto-commit mode when it is created.  As per the problem statement, an update was fired without explicitly disabling the auto-commit mode, the changes will be committed right after the update statement has finished execution.
-        
-        Changes to the database will be committed when another query (update or select) is fired using the connection.
+                
+        Q2
+                
+        Q2A1
+        Q2A2
+        Q2A3
+        Q2A4
+                
+        Q2A3
         """;
 
     final QuestionLoader questionLoader = new QuestionLoader();
 
     List<Question> questions = questionLoader.parse(inputText);
 
-    assertThat(questions.get(0).text())
-        .isEqualTo(
-            "Using a Callable would be more appropriate than using a Runnable in which of the following situations?");
+    final Question question1 = createQuestion("Q1", "Q1A2", "Q1A1", "Q1A2", "Q1A3", "Q1A4");
+    final Question question2 = createQuestion("Q2", "Q2A3", "Q2A1", "Q2A2", "Q2A3", "Q2A4");
 
-    assertThat(questions.get(1).answers().containsAll(
-    List.of(new Answer("Changes to the database are pending a commit call on the connection."),
-            new Answer(
-                "Changes to the database will be rolled back if another update is executed without commiting the previous update."),
-            new Answer(
-                "Changes to the database will be committed right after the update statement has completed execution."),
-            new Answer(
-                "A Connection is always in auto-commit mode when it is created.  As per the problem statement, an update was fired without explicitly disabling the auto-commit mode, the changes will be committed right after the update statement has finished execution.")
-    )));
+    assertThat(questions)
+        .containsExactly(question1, question2);
+  }
 
-    assertThat(questions.get(0).correctAnswer())
-        .isEqualTo(new Answer(
-            "When your task might throw a checked exception and you want to execute it in a separate Thread."));
-
-
-    assertThat(questions.get(1).text())
-        .isEqualTo(
-            "Given that a code fragment has just created a JDBC Connection and has executed an update statement, which of the following statements is correct?");
-
-            assertThat(questions.get(1).answers().containsAll(
-        List.of(new Answer("When you want to execute a task directly in a separate thread"),
-            new Answer(
-                "When your task might throw a checked exception and you want to execute it in a separate Thread"),
-            new Answer(
-                "When your task does not return any result but you want to execute the task asynchronously."),
-            new Answer(
-                "When you want to use ExecutorService to submit multiple instances of a task."))
-    ));
-
-    assertThat(questions.get(1).correctAnswer())
-        .isEqualTo(new Answer(
-            "Changes to the database will be committed when another query (update or select) is fired using the connection."));
-
+  private Question createQuestion(String questionText, String correctAnswer, String answer1,
+      String answer2, String answer3, String answer4) {
+    return new Question(questionText,
+        new MultipleChoice(
+            new Answer(correctAnswer),
+            List.of(
+                new Answer(answer1),
+                new Answer(answer2), new Answer(answer3),
+                new Answer(answer4))
+        ));
   }
 }
