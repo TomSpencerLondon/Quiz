d450914018dc21573d9ed11d9300a56f4836425b
Tom <thomas.spencer@codurance.com>
2021-11-26 16:47:39 +0000

    WIP - AddQuestion

--- src/main/java/com/example/quiz/adapter/in/web/AddQuestion.java
+++ src/main/java/com/example/quiz/adapter/in/web/AddQuestion.java
@@ -2,8 +2,10 @@ package com.example.quiz.adapter.in.web;
 
 import com.example.quiz.domain.Question;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
