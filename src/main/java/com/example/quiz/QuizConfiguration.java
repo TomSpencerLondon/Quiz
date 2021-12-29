81ea21234cf8ff929370fb4fae6e18ee2838743e
Tom <thomas.spencer@codurance.com>
2021-12-29 23:17:18 +0000

    Add afterMigrate and delete QuizConfiguration database update

--- src/main/java/com/example/quiz/QuizConfiguration.java
+++ src/main/java/com/example/quiz/QuizConfiguration.java
@@ -14,16 +14,6 @@ public class QuizConfiguration {
 
   @Bean
   Quiz createQuiz(QuestionRepository questionRepository) {
-    questionRepository.save(new Question(
-        "Question 1",
-        new MultipleChoice(new Answer("Answer 1"),
-            List.of(
-                new Answer("Answer 1"),
-                new Answer("Answer 2"),
-                new Answer("Answer 3"),
-                new Answer("Answer 4")
-                )
-    )));
     return new Quiz(questionRepository);
   }
 
