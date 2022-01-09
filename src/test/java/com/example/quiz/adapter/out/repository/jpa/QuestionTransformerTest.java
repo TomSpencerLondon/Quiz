0016f0f1817324514a8edd420473168a0a067c04
Tom <thomas.spencer@codurance.com>
2022-01-09 17:29:20 +0000

    Passing dboToQuestion test on transformer

---
+++ src/test/java/com/example/quiz/adapter/out/repository/jpa/QuestionTransformerTest.java
@@ -0,0 +1,49 @@
+package com.example.quiz.adapter.out.repository.jpa;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.example.quiz.domain.Choice;
+import com.example.quiz.domain.MultipleChoice;
+import com.example.quiz.domain.Question;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class QuestionTransformerTest {
+  final QuestionTransformer questionTransformer = new QuestionTransformer();
+
+  @Test
+  void turnQuestionDboToQuestion() {
+    // Given
+    final QuestionDbo questionDbo = new QuestionDbo();
+    questionDbo.setId(1L);
+    questionDbo.setText("Question 1");
+
+    final AnswerDbo answerDbo1 = new AnswerDbo();
+    answerDbo1.setQuestion(questionDbo);
+    answerDbo1.setChoiceText("Answer 1");
+    answerDbo1.setCorrect(true);
+
+    final AnswerDbo answerDbo2 = new AnswerDbo();
+    answerDbo2.setQuestion(questionDbo);
+    answerDbo2.setChoiceText("Answer 2");
+    answerDbo2.setCorrect(false);
+    questionDbo.addAnswer(answerDbo1);
+    questionDbo.addAnswer(answerDbo2);
+
+    final Question expected = new Question(
+        "Question 1",
+        new MultipleChoice(
+            new Choice("Answer 1"),
+            List.of(new Choice("Answer 1"), new Choice("Answer 2"))
+        )
+    );
+    expected.setId(1L);
+
+    // When
+    final Question question = questionTransformer.toQuestion(questionDbo);
+
+    // Then
+    assertThat(question)
+        .isEqualTo(expected);
+  }
+}
