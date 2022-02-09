f004274a813862cf23a14eff9c5f2d6955da83fe
Ted M. Young <tedyoung@gmail.com>
2022-02-09 14:42:15 -0800

    Added logging filter to show raw incoming body (payload) of POST and GET requests. You can turn this off by changing the logging level for the BeforeOnlyRequestLoggingFilter in the application.properties file.

---
+++ src/main/java/com/example/quiz/RequestLoggingFilterConfig.java
@@ -0,0 +1,22 @@
+package com.example.quiz;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.AbstractRequestLoggingFilter;
+
+@Configuration
+public class RequestLoggingFilterConfig {
+
+    @Bean
+    public AbstractRequestLoggingFilter logFilter() {
+        var filter = new BeforeOnlyRequestLoggingFilter();
+        filter.setIncludeQueryString(true);
+        filter.setIncludePayload(true);
+        filter.setMaxPayloadLength(1000);
+        filter.setIncludeClientInfo(false);
+        filter.setIncludeHeaders(false);
+        filter.setBeforeMessagePrefix("");
+        filter.setBeforeMessageSuffix("");
+        return filter;
+    }
+}
