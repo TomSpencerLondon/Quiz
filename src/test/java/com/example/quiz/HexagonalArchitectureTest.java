e005ad29b40ee36cfeaa9acfc69a21a91ca05dc9
Tom <thomas.spencer@codurance.com>
2022-02-19 10:36:59 +0000

    wip - Add ArchUnit - 29 violations for domain depending on adapter

---
+++ src/test/java/com/example/quiz/HexagonalArchitectureTest.java
@@ -0,0 +1,25 @@
+package com.example.quiz;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("architecture")
+public class HexagonalArchitectureTest {
+
+  @Test
+  void domainMustNotDependOnAdapters() {
+    noClasses()
+        .that().resideInAPackage("..domain..")
+        .should().dependOnClassesThat().resideInAPackage("..adapter..")
+        .check(productionAndTestClasses());
+  }
+
+
+  private JavaClasses productionAndTestClasses() {
+    return new ClassFileImporter().importPackages("com.example.quiz");
+  }
+}
