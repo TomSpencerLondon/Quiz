a00533929252d5c6acd9359062ef55d774410346
Tom <thomas.spencer@codurance.com>
2022-04-05 19:02:32 +0100

    Add starter for dynamic QuestionForm on add-question with form-fragments

--- src/main/java/com/example/quiz/adapter/in/web/edit/AddQuestionForm.java
+++ src/main/java/com/example/quiz/adapter/in/web/edit/AddQuestionForm.java
@@ -12,27 +12,22 @@ public class AddQuestionForm {
 
     @NotBlank
     private String text;
-    @Valid
-    private ChoiceForm choice1;
-    @Valid
-    private ChoiceForm choice2;
-    @Valid
-    private ChoiceForm choice3;
-    @Valid
-    private ChoiceForm choice4;
     @NotBlank
     private String choiceType;
+    private @Valid ChoiceForm[] choices;
 
     public AddQuestionForm() {
+        this.choices = new ChoiceForm[]{new ChoiceForm(), new ChoiceForm(), new ChoiceForm(), new ChoiceForm()};
     }
 
     public AddQuestionForm(String text, ChoiceForm choice1, ChoiceForm choice2,
                            ChoiceForm choice3, ChoiceForm choice4, String choiceType) {
         this.text = text;
-        this.choice1 = choice1;
-        this.choice2 = choice2;
-        this.choice3 = choice3;
-        this.choice4 = choice4;
+        this.choices = new ChoiceForm[4];
+        this.choices[0] = choice1;
+        this.choices[1] = choice2;
+        this.choices[2] = choice3;
+        this.choices[3] = choice4;
         this.choiceType = choiceType;
     }
 
@@ -41,19 +36,19 @@ public class AddQuestionForm {
     }
 
     public ChoiceForm getChoice1() {
-        return choice1;
+        return choices[0];
     }
 
     public ChoiceForm getChoice2() {
-        return choice2;
+        return choices[1];
     }
 
     public ChoiceForm getChoice3() {
-        return choice3;
+        return choices[2];
     }
 
     public ChoiceForm getChoice4() {
-        return choice4;
+        return choices[3];
     }
 
     public void setText(String text) {
@@ -61,19 +56,27 @@ public class AddQuestionForm {
     }
 
     public void setChoice1(ChoiceForm choice1) {
-        this.choice1 = choice1;
+        this.choices[0] = choice1;
     }
 
     public void setChoice2(ChoiceForm choice2) {
-        this.choice2 = choice2;
+        this.choices[1] = choice2;
     }
 
     public void setChoice3(ChoiceForm choice3) {
-        this.choice3 = choice3;
+        this.choices[2] = choice3;
     }
 
     public void setChoice4(ChoiceForm choice4) {
-        this.choice4 = choice4;
+        this.choices[3] = choice4;
+    }
+
+    public ChoiceForm[] getChoices() {
+        return choices;
+    }
+
+    public void setChoices(ChoiceForm[] choices) {
+        this.choices = choices;
     }
 
     public String getChoiceType() {
@@ -85,7 +88,7 @@ public class AddQuestionForm {
     }
 
     public List<String> transformToChoices() {
-        return Stream.of(choice1, choice2, choice3, choice4)
+        return Stream.of(choices)
                      .map(ChoiceForm::getChoice)
                      .toList();
     }
@@ -110,10 +113,9 @@ public class AddQuestionForm {
     }
 
     private List<String> correctChoices() {
-        List<String> correctChoices = Stream.of(choice1, choice2, choice3, choice4)
-                                            .filter(ChoiceForm::isCorrectAnswer)
-                                            .map(ChoiceForm::getChoice)
-                                            .toList();
-        return correctChoices;
+        return Stream.of(choices)
+                     .filter(ChoiceForm::isCorrectAnswer)
+                     .map(ChoiceForm::getChoice)
+                     .toList();
     }
 }
