07ae2f527edba2e7a71121ce3b2424fb327f6fa0
Tom <thomas.spencer@codurance.com>
2022-04-22 15:24:24 +0100

    Add choices for QuestionView with limit in thymeleaf

--- src/main/java/com/example/quiz/adapter/in/web/edit/QuestionView.java
+++ src/main/java/com/example/quiz/adapter/in/web/edit/QuestionView.java
@@ -3,25 +3,18 @@ package com.example.quiz.adapter.in.web.edit;
 import com.example.quiz.domain.Choice;
 import com.example.quiz.domain.Question;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class QuestionView {
-
     private final String text;
     private final String answer;
-    private final String choice1;
-    private final String choice2;
-    private final String choice3;
-    private final String choice4;
+    private final List<String> choices;
 
-    public QuestionView(String text, String answer, String choice1, String choice2,
-                        String choice3, String choice4) {
+    public QuestionView(String text, String answer, String... choices) {
         this.text = text;
         this.answer = answer;
-        this.choice1 = choice1;
-        this.choice2 = choice2;
-        this.choice3 = choice3;
-        this.choice4 = choice4;
+        this.choices = Arrays.stream(choices).toList();
     }
 
     public static QuestionView of(Question question) {
@@ -31,10 +24,7 @@ public class QuestionView {
             return new QuestionView(
                     question.text(),
                     correct.text(),
-                    question.choices().get(0).text(),
-                    question.choices().get(1).text(),
-                    question.choices().get(2).text(),
-                    question.choices().get(3).text()
+                    question.choices().stream().map(Choice::text).toArray(String[]::new)
             );
         } else {
             List<Choice> correct = question.choices().stream()
@@ -42,10 +32,7 @@ public class QuestionView {
             return new QuestionView(
                     question.text(),
                     correct.toString(),
-                    question.choices().get(0).text(),
-                    question.choices().get(1).text(),
-                    question.choices().get(2).text(),
-                    question.choices().get(3).text()
+                    question.choices().stream().map(Choice::text).toArray(String[]::new)
             );
         }
     }
@@ -58,20 +45,7 @@ public class QuestionView {
         return answer;
     }
 
-    public String getChoice1() {
-        return choice1;
+    public List<String> getChoices() {
+        return choices;
     }
-
-    public String getChoice2() {
-        return choice2;
-    }
-
-    public String getChoice3() {
-        return choice3;
-    }
-
-    public String getChoice4() {
-        return choice4;
-    }
-
 }
