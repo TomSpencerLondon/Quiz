d7e50ca88ed0fcfce3f13756e50c231ecd6f3d01
Tom <thomas.spencer@codurance.com>
2022-05-21 20:04:43 +0100

    Delete overcomplex config and replace with @CrossOrigin on API controller

--- src/main/java/com/example/quiz/adapter/in/api/QuizAPIController.java
+++ src/main/java/com/example/quiz/adapter/in/api/QuizAPIController.java
@@ -6,6 +6,7 @@ import com.example.quiz.application.QuestionService;
 import com.example.quiz.domain.Question;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class QuizAPIController {
     private final QuestionService questionService;
 
