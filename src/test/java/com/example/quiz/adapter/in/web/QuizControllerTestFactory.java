b697fbbdf83ced9f394a46bbc9a794eb93975a48
Tom <thomas.spencer@codurance.com>
2022-06-25 18:21:30 +0100

    Delete unused factories and remove unused functions from QuizControllerTestFactory

--- src/test/java/com/example/quiz/adapter/in/web/QuizControllerTestFactory.java
+++ src/test/java/com/example/quiz/adapter/in/web/QuizControllerTestFactory.java
@@ -1,32 +1,17 @@
 package com.example.quiz.adapter.in.web;
 
-import com.example.quiz.adapter.in.web.answer.QuizController;
-import com.example.quiz.adapter.in.web.answer.StubTokenGenerator;
-import com.example.quiz.hexagon.application.QuizService;
-import com.example.quiz.hexagon.application.QuizSessionService;
-import com.example.quiz.hexagon.application.port.*;
+import com.example.quiz.domain.factories.SingleChoiceQuestionTestFactory;
+import com.example.quiz.hexagon.application.port.InMemoryQuestionRepository;
+import com.example.quiz.hexagon.application.port.InMemoryQuizRepository;
+import com.example.quiz.hexagon.application.port.QuestionRepository;
+import com.example.quiz.hexagon.application.port.QuizRepository;
 import com.example.quiz.hexagon.domain.Question;
 import com.example.quiz.hexagon.domain.Quiz;
-import com.example.quiz.domain.factories.MultipleChoiceQuestionTestFactory;
-import com.example.quiz.domain.factories.SingleChoiceQuestionTestFactory;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
 public class QuizControllerTestFactory {
-    public static QuizController createAndStartQuizControllerWithOneSingleChoiceQuestion() {
-        QuestionRepository questionRepository = createQuestionRepositoryWithSingleChoiceQuestion();
-        QuizService quizService = new QuizService(questionRepository);
-        QuizRepository quizRepository = createQuizRepositoryWithOneQuizWith(questionRepository.findAll().get(0));
-        StubTokenGenerator stubIdGenerator = new StubTokenGenerator();
-
-        QuizSessionService quizSessionService =
-                new QuizSessionService(
-                        new InMemoryQuizSessionRepository(),
-                        quizRepository,
-                        stubIdGenerator);
-        return new QuizController(quizSessionService, questionRepository);
-    }
 
     @NotNull
     public static QuestionRepository createQuestionRepositoryWithSingleChoiceQuestion() {
@@ -41,14 +26,5 @@ public class QuizControllerTestFactory {
         QuizRepository quizRepository = new InMemoryQuizRepository();
         quizRepository.save(new Quiz("Test Quiz", List.of(question.getId())));
         return quizRepository;
-    }
-
-    public static QuizController createAndStartChoiceQuizControllerWithOneMultipleChoiceQuestion() {
-        QuestionRepository questionRepository = new InMemoryQuestionRepository();
-        Question savedQuestion = questionRepository.save(MultipleChoiceQuestionTestFactory.multipleChoiceQuestion());
-        QuizService quizService = new QuizService(questionRepository);
-        StubTokenGenerator stubIdGenerator = new StubTokenGenerator();
-        QuizSessionService quizSessionService = new QuizSessionService(new InMemoryQuizSessionRepository(), createQuizRepositoryWithOneQuizWith(savedQuestion), stubIdGenerator);
-        return new QuizController(quizSessionService, questionRepository);
     }
 }
